de66555310c4774862264e985999d6a2f8041542
SajithKavinda99 <sajithbusiness094@gmail.com>
2024-05-17 20:30:53 +0530

    first commit

---
+++ src/studentregister/DBConnection.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentregister;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author dell
+ */
+public class DBConnection {
+    
+    public static  Connection connect() throws SQLException{
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/studentsregister","root","");
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println(conn);
+        
+        return conn;
+        
+    }
+
+    static Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/studentregister/studentregister.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentregister;
+
+/**
+ *
+ * @author dell
+ */
+public class studentregister {
+    public static void main(String[] args) {
+        
+      systemlogin start = new systemlogin();
+      start.setVisible(true);
+       
+        
+    }
+    
+}
